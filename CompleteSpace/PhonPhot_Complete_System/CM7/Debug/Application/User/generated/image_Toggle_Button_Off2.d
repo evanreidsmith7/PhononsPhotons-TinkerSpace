a3bd8535976f3f6f44a16e9defde07666bd288fd
Evan Smith <ers131@txstate.edu>
2024-02-02 14:41:28 -0600

    successful build in ioc project
    since the cm7 project kept getting destroyed by cubemx after code generation, i generated code, then replaced cm7 with the cm7 from "localspace" and its now building. I have not debugged but i assume it probably would be ok to

---
+++ CompleteSpace/PhonPhot_Complete_System/CM7/Debug/Application/User/generated/image_Toggle_Button_Off2.d
@@ -0,0 +1,4 @@
+Application/User/generated/image_Toggle_Button_Off2.o: \
+ C:/Users/Owner/MegaMachineSTM32Work/PhononsPhotons-TinkerSpace/CompleteSpace/PhonPhot_Complete_System/TouchGFX/generated/images/src/image_Toggle_Button_Off2.cpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
---
+++ CompleteSpace/PhonPhot_Complete_System/CM7/Debug/Application/User/generated/image_Toggle_Button_On2.d
@@ -0,0 +1,4 @@
+Application/User/generated/image_Toggle_Button_On2.o: \
+ C:/Users/Owner/MegaMachineSTM32Work/PhononsPhotons-TinkerSpace/CompleteSpace/PhonPhot_Complete_System/TouchGFX/generated/images/src/image_Toggle_Button_On2.cpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
