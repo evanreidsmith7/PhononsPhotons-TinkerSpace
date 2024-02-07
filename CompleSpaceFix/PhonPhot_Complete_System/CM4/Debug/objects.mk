c340ed12daf148f74ea468d2a16acc935224b4a3
Evan Smith <ers131@txstate.edu>
2024-02-07 11:32:30 -0600

    completespacefix (changes from evanpc pushing to main)

---
+++ CompleSpaceFix/PhonPhot_Complete_System/CM4/Debug/objects.mk
@@ -0,0 +1,9 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+USER_OBJS := C:/Users/evanr/MiniMachineSTM32Work/PhononsPhotons-TinkerSpace/CompleSpaceFix/PhonPhot_Complete_System/CM4/Lib/libARM_Math_STM32G4_Library.a
+
+LIBS := -lARM_Math_STM32G4_Library
+
