15e0d91a0fdd683535220dd8079c057957b5eea8
Evan Smith <ers131@txstate.edu>
2024-02-07 11:31:02 -0600

    completespacefix, debug successfull

---
+++ CompleSpaceFix/PhonPhot_Complete_System/CM7/Debug/Application/User/Core/Src/ipc_interface.d
@@ -0,0 +1,6 @@
+Application/User/Core/Src/ipc_interface.o: \
+ ../Application/User/Core/Src/ipc_interface.c \
+ ../Application/User/Core/Inc/ipc_interface.h \
+ ../Application/User/Core/Inc/common.h
+../Application/User/Core/Inc/ipc_interface.h:
+../Application/User/Core/Inc/common.h:
