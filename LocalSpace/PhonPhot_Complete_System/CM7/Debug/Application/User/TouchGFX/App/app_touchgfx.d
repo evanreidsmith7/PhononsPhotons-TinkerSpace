a9d172e889d1a752378f8f6cb30f5226eb7f2bd5
agrahm <agrahm@swri.org>
2024-02-07 11:33:28 -0600

    Working Builds

---
+++ LocalSpace/PhonPhot_Complete_System/CM7/Debug/Application/User/TouchGFX/App/app_touchgfx.d
@@ -0,0 +1,4 @@
+Application/User/TouchGFX/App/app_touchgfx.o: \
+ C:/Projects/PhononsPhotons-TinkerSpace/LocalSpace/PhonPhot_Complete_System/TouchGFX/App/app_touchgfx.c \
+ C:/Projects/PhononsPhotons-TinkerSpace/LocalSpace/PhonPhot_Complete_System/TouchGFX/App/app_touchgfx.h
+C:/Projects/PhononsPhotons-TinkerSpace/LocalSpace/PhonPhot_Complete_System/TouchGFX/App/app_touchgfx.h:
---
+++ LocalSpace/PhonPhot_Complete_System/CM7/Debug/Drivers/Components/ft5336_reg.d
@@ -0,0 +1,4 @@
+Drivers/Components/ft5336_reg.o: \
+ C:/Projects/PhononsPhotons-TinkerSpace/LocalSpace/PhonPhot_Complete_System/Drivers/BSP/Components/ft5336/ft5336_reg.c \
+ C:/Projects/PhononsPhotons-TinkerSpace/LocalSpace/PhonPhot_Complete_System/Drivers/BSP/Components/ft5336/ft5336_reg.h
+C:/Projects/PhononsPhotons-TinkerSpace/LocalSpace/PhonPhot_Complete_System/Drivers/BSP/Components/ft5336/ft5336_reg.h:
