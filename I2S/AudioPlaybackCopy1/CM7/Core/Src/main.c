c3592fe12f851ae624cd577a7e0343d93e9706fe
Evan Smith <ers131@txstate.edu>
2024-02-21 12:29:33 -0600

    cache enables, need to fix library not found error

--- I2S/AudioPlaybackCopy1/CM7/Core/Src/main.c
+++ I2S/AudioPlaybackCopy1/CM7/Core/Src/main.c
@@ -75,6 +75,13 @@ int main(void)
 /* USER CODE BEGIN Boot_Mode_Sequence_0 */
   int32_t timeout;
 /* USER CODE END Boot_Mode_Sequence_0 */
+/* Enable the CPU Cache */
+
+  /* Enable I-Cache---------------------------------------------------------*/
+  SCB_EnableICache();
+
+  /* Enable D-Cache---------------------------------------------------------*/
+  SCB_EnableDCache();
 
 /* USER CODE BEGIN Boot_Mode_Sequence_1 */
   /* Wait until CPU2 boots and enters in stop mode or timeout*/
