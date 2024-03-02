c2960a5bc14a86e718ab76556f87d28ffa706e65
Evan Smith <ers131@txstate.edu>
2024-03-02 15:19:34 -0600

    begin record (figure out data format)

--- ArduinoWorkspace/i2sInput/pythoncollect/save_to_wave.py
+++ ArduinoWorkspace/i2sInput/pythoncollect/save_to_wave.py
@@ -2,48 +2,11 @@ import serial
 import datetime
 import wave
 
+# open a serial connection to the Arduino
+ser = serial.Serial('COM16', 115200)
 
 # ask the user for the length of the audio in seconds
 length = int(input("Enter the length of the recording in seconds: "))
-
-
-# set the WAV file parameters
-channels = 1
-sample_rate = 16000
-
-# Specify the COM port and baud rate
-comPort = "COM13"
-baudRate = 115200
-
-# Create a new SerialPort object
-serialPort = serial.Serial(comPort, baudRate)
-
-
-
-
-try:
-    while True:
-        data = serialPort.readline().decode('utf-8').strip()
-        print(data)
-except serial.SerialException as e:
-    print(f"Error: {e}")
-except KeyboardInterrupt:
-    print("Serial reading terminated by the user.")
-finally:
-    serialPort.close()
-
-
-
-
-
-
-
-'''
-for i in range(length*sample_rate):
-   data = ser.read(4)
-   print(data)
-
-
 
 # get the current date and time
 now = datetime.datetime.now()
@@ -51,8 +14,10 @@ now = datetime.datetime.now()
 # create a filename with a datetimestamp
 filename = "recording_{}.wav".format(now.strftime("%Y-%m-%d_%H-%M-%S"))
 
-
+# set the WAV file parameters
+channels = 1
 sample_width = 2 # 2 bytes per sample
+sample_rate = 16000
 
 # open a file for writing the audio data
 with wave.open(filename, 'wb') as f:
@@ -71,4 +36,3 @@ with wave.open(filename, 'wb') as f:
 ser.close()
 
 print("Recording saved as {}".format(filename))
-'''
