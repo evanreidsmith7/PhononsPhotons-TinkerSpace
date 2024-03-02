0b080e1f09f23616bd66a4d4e69dc79426af1d5b
Evan Smith <ers131@txstate.edu>
2024-03-01 18:55:22 -0600

    starting collect uart

--- ArduinoWorkspace/i2sInput/i2s_in/i2s_in.ino
+++ ArduinoWorkspace/i2sInput/i2s_in/i2s_in.ino
@@ -9,7 +9,7 @@
 #define I2S_PORT I2S_NUM_0
 
 // Define input buffer length
-#define bufferLen 512
+#define bufferLen 1024
 uint32_t sBuffer[bufferLen];
 
 void i2s_install() 
@@ -18,7 +18,7 @@ void i2s_install()
   const i2s_config_t i2s_config = 
   {
     .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
-    .sample_rate = 44000,
+    .sample_rate = 16000,
     .bits_per_sample = i2s_bits_per_sample_t(32),
     .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
     .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
