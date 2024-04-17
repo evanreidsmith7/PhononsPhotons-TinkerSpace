48922fca8af7cb0be6f4ef55ac5b6b0b88cde7da
agram20 <102429658+agram20@users.noreply.github.com>
2024-04-17 10:54:34 -0500

    Working inference engine/GUI updates

---
+++ CompleSpaceFix/replace_after_regen/Localize_AnomalyView.cpp
@@ -0,0 +1,35 @@
+#include <gui/localize_anomaly_screen/Localize_AnomalyView.hpp>
+#include <BitmapDatabase.hpp>
+
+static bool toggle = false;
+
+Localize_AnomalyView::Localize_AnomalyView()
+{
+
+}
+
+void Localize_AnomalyView::setupScreen()
+{
+    Localize_AnomalyViewBase::setupScreen();
+}
+
+void Localize_AnomalyView::tearDownScreen()
+{
+    Localize_AnomalyViewBase::tearDownScreen();
+}
+
+void Localize_AnomalyView::update_image()
+{
+	if(toggle == true)
+    {
+	  //Directional_Vector.setBitmap(Bitmap(BITMAP_KEEPHERNAMEOUTYOURMOUTH_ID));
+	  Directional_Vector.invalidate();
+    }
+	else
+	{
+	  //Directional_Vector.setBitmap(Bitmap(BITMAP_VECTOR_IMAGE_ID));
+	  Directional_Vector.invalidate();
+	}
+	toggle = !toggle;
+
+}
